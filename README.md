71ccc94463b2c2c06c6bb386e35eca3224867aef
Matthieu Isoard <matthieu.isoard@atos.net>
2022-03-30 14:41:26 +0200

    init repo

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # quantum
+
+This repo contains several examples of Quantum ... just for fun.
+
+## qiskit-myqlm-1
+
+An image generator based on qiskit and myqlm
+
+![qiskit-myqlm-1](images/qiskit-myqlm-1.png?raw=true "qiskit-myqlm-1")
+
+```
+#Make sure to get qiskit, myqlm, myqlm-interop, tk ...
+$ python3 ./qiskit-myqlm-1.py
+
+```
---
+++ qiskit-myqlm-1.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+from qiskit import *
+from qiskit.tools.monitor import job_monitor
+from qat.interop.qiskit import qiskit_to_qlm
+from qat.pylinalg import PyLinalg
+
+import turtle
+from turtle import *
+import math
+
+
+qr = QuantumRegister(5)
+cr = ClassicalRegister(5)
+
+circuit=QuantumCircuit(qr,cr)
+circuit.h(qr[0])
+circuit.h(qr[1])
+circuit.h(qr[2])
+circuit.h(qr[3])
+circuit.h(qr[4])
+
+circuit.cx(qr[0],qr[1])
+circuit.measure(qr,cr)
+
+qlm_circuit = qiskit_to_qlm(circuit)
+
+qlm_job = qlm_circuit.to_job()
+qlm_qpu = PyLinalg() # or any other QPU you would like to raise
+qlm_result = qlm_qpu.submit(qlm_job)
+
+bin=qlm_result[0].state
+
+print(str(bin)[1:-1])
+dec=int(str(bin)[1:-1], base=2)
+print(dec)
+
+nft=turtle.Turtle()
+nft.color("blue")
+nft.speed(10)
+title("Quantum demo")
+
+for i in range((dec+1)*30):
+    nft.forward(math.sqrt(i)*20)
+    nft.left(dec*10)
+    nft.right(dec/2)
+
+turtle.done
