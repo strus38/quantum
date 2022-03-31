d8498adac1bd1f7503aa3b5aadc05cb994f8b6c6
Matthieu Isoard <matthieu.isoard@atos.net>
2022-03-31 10:19:09 +0200

    adding JARVICE support

---
+++ image-common-master/README.md
@@ -0,0 +1,102 @@
+# image-common
+Common packages and configuration files for Nimbix base images. This will
+retrofit existing Docker images for better execution on JARVICE.
+
+If you have an image derived from a non-Nimbix base, and you want to improve
+its execution on JARVICE, there is now a simple way to do this in your
+Dockerfile without having to change your FROM line.  Currently, we only
+support CentOS and Ubuntu with this trick.
+
+Verified distributions:
+* CentOS 7 (x86_64, ppc64le)
+* Ubuntu 16.04 Xenial (amd64, ppc64le)
+* Ubuntu 18.04 Bionic (amd64, ppc64le)
+
+Just add this to the end of your Dockerfile:
+
+# Ubuntu
+```bash
+RUN apt-get -y update && \
+    DEBIAN_FRONTEND=noninteractive apt-get -y install curl && \
+    curl -H 'Cache-Control: no-cache' \
+        https://raw.githubusercontent.com/nimbix/image-common/master/install-nimbix.sh \
+        | bash
+
+# Expose port 22 for local JARVICE emulation in docker
+EXPOSE 22
+```
+
+# Ubuntu (with Nimbix desktop)
+```bash
+RUN apt-get -y update && \
+    DEBIAN_FRONTEND=noninteractive apt-get -y install curl && \
+    curl -H 'Cache-Control: no-cache' \
+        https://raw.githubusercontent.com/nimbix/image-common/master/install-nimbix.sh \
+        | bash -s -- --setup-nimbix-desktop
+
+# Expose port 22 for local JARVICE emulation in docker
+EXPOSE 22
+
+# for standalone use
+EXPOSE 5901
+EXPOSE 443
+```
+
+# CentOS
+```bash
+RUN curl -H 'Cache-Control: no-cache' \
+        https://raw.githubusercontent.com/nimbix/image-common/master/install-nimbix.sh \
+        | bash
+
+# Expose port 22 for local JARVICE emulation in docker
+EXPOSE 22
+```
+
+# CentOS (with Nimbix desktop)
+```bash
+RUN curl -H 'Cache-Control: no-cache' \
+        https://raw.githubusercontent.com/nimbix/image-common/master/install-nimbix.sh \
+        | bash -s -- --setup-nimbix-desktop
+
+# Expose port 22 for local JARVICE emulation in docker
+EXPOSE 22
+
+# for standalone use
+EXPOSE 5901
+EXPOSE 443
+```
+
+This does several things:
+ 1. installs recommended packages for running on JARVICE
+ 2. makes sure SSH actually starts (important when using `nvidia/` base images for example)
+ 3. sets up the nimbix user, and gives it password-less sudo access
+ 4. configures JARVICE "emulation" for local testing (see our PushToCompute tutorial for more on that)
+ 5. preserves the Docker environment variables and makes sure they are set when you run in JARVICE as well
+
+# Docker build caching of **install-nimbix.sh** layer
+
+While developing an app and subsequently running multiple docker builds, it
+can be easy to be thrown off by the caching of successfully built layers.
+Docker will not be able to pick up changes to **install-nimbix.sh** in the
+**image-common** git repository.  Thus, if the layer's **RUN** instruction has
+not changed, it may not be rebuilt if it has previously executed successfully.
+
+Using an easily updatable **ENV** instruction is a simple way to invalidate
+a layer and force it to be rebuilt.  Simply add an instruction like this
+before your **RUN** of **install-nimbix.sh** or any other layers
+you wish to easily invalidate by changing the **SERIAL_NUMBER**:
+```bash
+# Update SERIAL_NUMBER to force rebuild of subsequent layers
+#   (i.e. don't use cached layers)
+ENV SERIAL_NUMBER 20171009.1713
+```
+
+Allow more control by using **--build-arg** with a local **docker build**
+command line:
+```bash
+# Update SERIAL_NUMBER to force rebuild of subsequent layers
+#   (i.e. don't use cached layers)
+ARG SERIAL_NUMBER
+ENV SERIAL_NUMBER ${SERIAL_NUMBER:-20171009.1713}
+```
+
---
+++ image-common-master/install-nimbix.sh
@@ -0,0 +1,151 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+ARCH=$(arch)
+BRANCH=master
+
+while [ $# -gt 0 ]; do
+  case $1 in
+  --setup-nimbix-desktop)
+    SETUP_NIMBIX_DESKTOP=1
+    shift
+    ;;
+  --setup-realvnc)
+    SETUP_REALVNC=1
+    shift
+    ;;
+  --disable-desktop-autostart)
+    export DISABLE_DESKTOP_AUTOSTART=1
+    shift
+    ;;
+  --skip-os-pkg-update)
+    export SKIP_OS_PKG_UPDATE=1
+    shift
+    ;;
+  --skip-mpi-pkg)
+    export SKIP_MPI_PKG=1
+    shift
+    ;;
+  --image-common-branch)
+    BRANCH=$2
+    shift
+    shift
+    ;;
+  *)
+    break
+    ;;
+  esac
+done
+
+# Base OS
+function setup_base_os() {
+  PKGS="curl zip unzip sudo"
+  
+  # install EPEL first, successive packages live there
+  dnf -y install epel-release
+
+  # Packages to support MPI and basic container operation
+  PKGS+=" passwd xz tar file openssh-server openssh-clients python3"
+  PKGS+=" which sshpass mailcap initscripts"
+  if [[ -z $SKIP_MPI_PKG ]]; then
+    PKGS+=" openmpi openmpi3 perftest"
+    PKGS+=" dapl compat-dapl dapl.i686 compat-dapl.i686 infiniband-diags"
+    PKGS+=" rdma-core rdma-core.i686 libibverbs libibverbs-utils"
+  fi
+  [ -z "$SKIP_OS_PKG_UPDATE" ] && dnf -y update
+  dnf -y install $PKGS
+
+  # Set locale
+  localedef -i en_US -f UTF-8 en_US.UTF-8
+
+  echo '# leave empty' >/etc/fstab
+}
+
+# Nimbix JARVICE emulation
+function setup_jarvice_emulation() {
+  cd /tmp
+  curl https://codeload.github.com/nimbix/image-common/zip/$BRANCH \
+    >/tmp/nimbix.zip
+  unzip nimbix.zip
+  rm -f nimbix.zip
+  /tmp/nimbix/setup-nimbix.sh
+
+  # Redundant directory copies, use a soft link, favor the /usr/local/ but
+  #  J2 depends on this so allow the full copies for now
+  mkdir -p /usr/lib/JARVICE
+  cp -a /tmp/nimbix/tools /usr/lib/JARVICE
+  mkdir -p /usr/local/JARVICE
+  cp -a /tmp/nimbix/tools /usr/local/JARVICE
+  #    ln -sf /usr/local/JARVICE /usr/lib/JARVICE
+  cat <<'EOF' | tee /etc/profile.d/jarvice-tools.sh >/dev/null
+JARVICE_TOOLS="/usr/local/JARVICE/tools"
+JARVICE_TOOLS_BIN="$JARVICE_TOOLS/bin"
+PATH="$PATH:$JARVICE_TOOLS_BIN"
+export JARVICE_TOOLS JARVICE_TOOLS_BIN PATH
+EOF
+
+  cd /tmp
+  mkdir -p /etc/JARVICE
+  cp -a /tmp/nimbix/etc/* /etc/JARVICE
+  chmod 755 /etc/JARVICE
+  mkdir -m 0755 /data
+  chown nimbix:nimbix /data
+}
+
+function setup_nimbix_desktop() {
+  mkdir -p /usr/local/lib/nimbix_desktop
+
+  # Copy in the VNC server installers, both for CentOS, and the XFCE files
+  files="install-centos-desktop.sh"
+  files+=" install-centos-real.sh help-real.html"
+  
+  files+=" prep-tiger.sh install-tiger.sh help-tiger.html postinstall-desktop.sh"
+  files+=" nimbix_desktop url.txt xfce4-session-logout share skel.config mimeapps.list helpers.rc"
+
+  # Pull the files from the install bolus
+  for i in $files; do
+    cp -a /tmp/nimbix/nimbix_desktop/"$i" \
+      /usr/local/lib/nimbix_desktop
+  done
+
+  # Install RealVNC server on CentOS if requested, setup the desktop files
+  
+  /usr/local/lib/nimbix_desktop/install-centos-desktop.sh
+
+  if [[ -n "$SETUP_REALVNC" ]]; then
+    /usr/local/lib/nimbix_desktop/install-centos-real.sh
+  fi
+
+  if [[ $ARCH == x86_64 ]]; then
+    /usr/local/lib/nimbix_desktop/prep-tiger.sh
+  fi
+
+  if [[ -z "$SETUP_REALVNC" ]]; then
+    cp /usr/local/lib/nimbix_desktop/help-tiger.html /etc/NAE/help.html
+  fi
+
+  # clean up older copies, make a link for all apps to find nimbix_desktop
+  rm -f /usr/lib/JARVICE/tools/nimbix_desktop
+  ln -sf /usr/local/lib/nimbix_desktop/ /usr/lib/JARVICE/tools/nimbix_desktop
+
+  # recreate nimbix user home to get the right skeleton files
+  /bin/rm -rf /home/nimbix
+  /sbin/mkhomedir_helper nimbix
+
+  # Add a marker file for using a local, updated noVNC install
+  echo /usr/local/JARVICE/tools/noVNC | sudo tee /etc/.novnc-stable
+}
+
+function cleanup() {
+  dnf clean all
+  rm -rf /tmp/nimbix
+}
+
+setup_base_os
+setup_jarvice_emulation
+[ -n "$SETUP_NIMBIX_DESKTOP" ] && setup_nimbix_desktop
+cleanup
+
+exit 0
